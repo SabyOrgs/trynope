38eb7f8bcc0e8a11518985920f4fbf86365e24d8
SabyOrgs <me@saby.org>
2024-06-05 08:20:32 +0530

    first commit

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -1,9 +1,9 @@
-import * as React from "react"
+import * as React from "react";
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { Icons } from "@/components/icons"
-import { ModeToggle } from "@/components/mode-toggle"
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
@@ -14,39 +14,11 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
           <p className="text-center text-sm leading-loose md:text-left">
             Built by{" "}
             <a
-              href={siteConfig.links.twitter}
-              target="_blank"
+              href="#"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              shadcn
-            </a>
-            . Hosted on{" "}
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Vercel
-            </a>
-            . Illustrations by{" "}
-            <a
-              href="https://popsy.co"
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              Popsy
-            </a>
-            . The source code is available on{" "}
-            <a
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
+              Nope
             </a>
             .
           </p>
@@ -54,5 +26,5 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <ModeToggle />
       </div>
     </footer>
-  )
+  );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,38 +1,37 @@
-import { ClassValue, clsx } from "clsx"
-import { Metadata } from "next"
-import { env } from "process"
-import { twMerge } from "tailwind-merge"
-
+import { ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
+import { env } from "process";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function formatDate(input: string | number): string {
-  const date = new Date(input)
+  const date = new Date(input);
   return date.toLocaleDateString("en-US", {
     month: "long",
     day: "numeric",
     year: "numeric",
-  })
+  });
 }
 
 export function absoluteUrl(path: string) {
-  return `${env.NEXT_PUBLIC_APP_URL}${path}`
+  return `${env.NEXT_PUBLIC_APP_URL}${path}`;
 }
 
 export function constructMetadata({
-  title = "RIV.app - RiverTree AI",
-  description = "Transform Your Data into Conversations with RiverTree AI",
+  title = "Nope Studio - trynope.com",
+  description = "Nope is your go-to solution for all things tech and beyond.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
-  noIndex = false
+  noIndex = false,
 }: {
-  title?: string
-  description?: string
-  image?: string
-  icons?: string
-  noIndex?: boolean
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
 } = {}): Metadata {
   return {
     title,
@@ -42,24 +41,24 @@ export function constructMetadata({
       description,
       images: [
         {
-          url: image
-        }
-      ]
+          url: image,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
       title,
       description,
       images: [image],
-      creator: "@sebastin"
+      creator: "@sebastin",
     },
     icons,
-    metadataBase: new URL('https://riv.app'),
+    metadataBase: new URL("https://trynope.com"),
     ...(noIndex && {
       robots: {
         index: false,
-        follow: false
-      }
-    })
-  }
+        follow: false,
+      },
+    }),
+  };
 }
